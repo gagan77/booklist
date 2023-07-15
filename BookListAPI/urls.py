9c5d9c2f4d3d5874cd969356465ed18a9f0d72ee
gcheema77 <gcheema7@hotmail.com>
2023-07-15 11:51:16 -0600

    Added debug toolbar

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.urls import include
 urlpatterns = [
     # path('books/',views.books)
     path('books',views.BookList.as_view()),
     path('books/<int:pk>',views.Book.as_view()),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
