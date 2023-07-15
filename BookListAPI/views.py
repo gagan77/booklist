9a2831a1342a44d5bbb59e88e66312d8b64689d0
gcheema77 <gcheema7@hotmail.com>
2023-07-15 10:59:39 -0600

    tested http response vs api_view

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def books(request):
     return Response("list of the books", status=status.HTTP_200_OK)
